d477666813f98433b1414f39d595f9d7a6ed94bb
TaekyungHa <86tkstar@gmail.com>
2025-08-11 22:33:08 +0900

    add NativeWind for styling and complete initial setup

---
+++ src/app/(app)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Page() {
+	return (
+		<View className="flex-1 justify-center items-center">
+			<Text className="text-red-400">First page</Text>
+		</View>
+	);
+}

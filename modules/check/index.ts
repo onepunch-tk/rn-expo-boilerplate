fc890aced0875475ce9d7c1a72dda9c29c451c68
TaekyungHa <86tkstar@gmail.com>
2025-08-19 13:56:08 +0900

    implement auth flow ui and kakao sdk auth setup

--- modules/check/index.ts
+++
@@ -1,5 +0,0 @@
-// Reexport the native module. On web, it will be resolved to CheckModule.web.ts
-// and on native platforms to CheckModule.ts
-export { default } from './src/CheckModule';
-export { default as CheckView } from './src/CheckView';
-export * from  './src/Check.types';
